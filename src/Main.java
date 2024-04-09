a8809d3d5384bca5d0efe31acb247a7afda0418c
LY6861 <l19524827591y@163.com>
2024-04-09 14:37:58 +0800

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,6 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.InputMismatchException;
-import java.util.Scanner;
-
-// 按两次 Shift 打开“随处搜索”对话框并输入 `show whitespaces`，
-// 然后按 Enter 键。现在，您可以在代码中看到空格字符。
 public class Main {
     public static void main(String[] args) throws Exception{
         ServerSocket server=new ServerSocket(8080);
