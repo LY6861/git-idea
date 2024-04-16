a0963a5a00eb456be71877b72254c13f6fc06c51
LY6861 <149241821+LY6861@users.noreply.github.com>
2024-04-16 15:13:30 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("git-idea");
         System.out.println("2024.4.16");
+        System.out.println("22物联网03");
+        System.out.println("刘毅");
+        System.out.println("2022133347");
     }
 }
