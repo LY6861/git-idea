a811abbc3900efefbcee69d6f590efa801d2b67e
LY6861 <l19524827591y@163.com>
2024-04-16 14:29:04 +0800

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,6 @@
-import java.io.*;
-import java.net.ServerSocket;
-import java.net.Socket;
 public class Main {
-    public static void main(String[] args) throws Exception{
-        ServerSocket server=new ServerSocket(8080);
-        System.out.println("Listening for connection on port 8080...");
-        while (true){
-             Socket clientSocket=server.accept();
-            InputStreamReader isr=new InputStreamReader(clientSocket.getInputStream());
-            BufferedReader reader=new BufferedReader(isr);
-            String line=reader.readLine();
-            while (!line.isEmpty()){
-                System.out.println(line);
-                line=reader.readLine();
-            }
-        }
+    public static void main(String[] args) {
+        System.out.println("git-idea");
+        System.out.println("2024.4.16");
     }
 }
